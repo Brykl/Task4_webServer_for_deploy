711c4d2b03fd458f8df8f943b0f8ea1e5bb0b17a
Brykl <153744203+Brykl@users.noreply.github.com>
2024-04-27 03:41:42 +0500

    change server2

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'It\'s work'})
+})
+
 // mongoose.connect('mongodb://127.0.0.1:27017/employee');
 
 
