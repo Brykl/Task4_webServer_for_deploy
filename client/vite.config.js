7a76082d654885710921017ed9f17120edc9581c
Brykl <153744203+Brykl@users.noreply.github.com>
2024-04-28 01:06:55 +0500

    Changed vite.conf

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://task4-web-server-for-deploy-server.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
